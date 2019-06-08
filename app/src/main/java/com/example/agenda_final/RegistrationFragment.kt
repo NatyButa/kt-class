9fbea437ef3094c29b06a13cc8871f76e3ad0f8a
naty <tarekegnnathenael@gmail.com>
2019-06-08 14:58:19 -0700

    firebase configuration and registration fragment

---
+++ app/src/main/java/com/example/agenda_final/RegistrationFragment.kt
@@ -0,0 +1,63 @@
+package com.example.agenda_final
+
+
+import android.app.ProgressDialog
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.fragment_registration.*
+
+
+class RegistrationFragment : Fragment() {
+
+    private var etFirstName: EditText? = null
+    private var etLastName: EditText? = null
+    private var etEmail: EditText? = null
+    private var etPassword: EditText? = null
+    private var btnCreateAccount: Button? = null
+    private var mProgressBar: ProgressDialog? = null
+
+    //Firebase references
+    private var mDatabaseReference: DatabaseReference? = null
+    private var mDatabase: FirebaseDatabase? = null
+    private var mAuth: FirebaseAuth? = null
+
+    private var firstName: String? = null
+    private var lastName: String? = null
+    private var email: String? = null
+    private var password: String? = null
+
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_registration, container, false)
+    }
+
+    private fun initialise() {
+        etFirstName = et_first_name
+        etLastName = et_last_name
+        etEmail =et_email
+        etPassword = et_password
+        btnCreateAccount = btn_register
+        mProgressBar = ProgressDialog(activity)
+        mDatabase = FirebaseDatabase.getInstance()
+        mDatabaseReference = mDatabase!!.reference!!.child("Users")
+        mAuth = FirebaseAuth.getInstance()
+//        btnCreateAccount!!.setOnClickListener { createNewAccount()
+    }
+}
+
+
+}
