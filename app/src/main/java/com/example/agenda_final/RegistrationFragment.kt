142c932a5c2219a493c1bdf293f4bd0c599a3b49
naty <tarekegnnathenael@gmail.com>
2019-06-10 01:47:21 -0700

    data class added

--- app/src/main/java/com/example/agenda_final/RegistrationFragment.kt
+++ app/src/main/java/com/example/agenda_final/RegistrationFragment.kt
@@ -60,4 +60,4 @@ class RegistrationFragment : Fragment() {
 }
 
 
-}
+
--- app/src/main/java/com/example/agenda_final/data/Agenda.kt
+++ app/src/main/java/com/example/agenda_final/data/Agenda.kt
@@ -1,16 +1,16 @@
-package com.example.agenda_final.data
-
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-import java.io.Serializable
-
-
-@Entity(tableName = "agendas")
-data class Agenda(
-    @PrimaryKey @ColumnInfo(name = "memo_number") val memo:Int,
-    @ColumnInfo(name = "title")val title:String,
-    @ColumnInfo(name = "body")val body:String,
-    @ColumnInfo(name = "tag")val tag:String):Serializable{
-
-}
+//package com.example.agenda_final.data
+//
+//import androidx.room.ColumnInfo
+//import androidx.room.Entity
+//import androidx.room.PrimaryKey
+//import java.io.Serializable
+//
+//
+//@Entity(tableName = "agendas")
+//data class Agenda(
+//    @PrimaryKey @ColumnInfo(name = "memo_number") val memo:Int,
+//    @ColumnInfo(name = "title")val title:String,
+//    @ColumnInfo(name = "body")val body:String,
+//    @ColumnInfo(name = "tag")val tag:String):Serializable{
+//
+//}
