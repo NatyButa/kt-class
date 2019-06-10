48064039a830df657514cf61341120536e7880ff
naty <tarekegnnathenael@gmail.com>
2019-06-10 01:48:29 -0700

    database configuration added using room

--- app/src/main/java/com/example/agenda_final/data/AgendaDao.kt
+++ app/src/main/java/com/example/agenda_final/data/AgendaDao.kt
@@ -1,21 +1,21 @@
-package com.example.agenda_final.data
-
-import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
-import com.example.agenda_final.data.Agenda as Agenda
-
-
-@Dao
-interface AgendaDao{
-
-    @Query("SELECT * FROM agendas ORDER BY memo_number")
-    fun getAllAgenda(): LiveData<List<Agenda>>
-
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertAgenda(agenda: Agenda):Long
-
-}
+//package com.example.agenda_final.data
+//
+//import androidx.lifecycle.LiveData
+//import androidx.room.Dao
+//import androidx.room.Insert
+//import androidx.room.OnConflictStrategy
+//import androidx.room.Query
+//import com.example.agenda_final.data.Agenda as Agenda
+//
+//
+//@Dao
+//interface AgendaDao{
+//
+//    @Query("SELECT * FROM agendas ORDER BY memo_number")
+//    fun getAllAgenda(): LiveData<List<Agenda>>
+//
+//
+//    @Insert(onConflict = OnConflictStrategy.REPLACE)
+//    fun insertAgenda(agenda: Agenda):Long
+//
+//}
--- app/src/main/java/com/example/agenda_final/data/AgendaDatabase.kt
+++ app/src/main/java/com/example/agenda_final/data/AgendaDatabase.kt
@@ -1,39 +1,39 @@
-package com.example.agenda_final.data
-
-import android.content.Context
-import androidx.room.Database
-import androidx.room.Room
-import androidx.room.RoomDatabase
-
-
-@Database(entities = arrayOf(Agenda::class),version = 1)
-abstract class AgendaDatabase: RoomDatabase(){
-
-    abstract fun agendaDao():AgendaDao
-
-    companion object{
-
-        @Volatile
-        private var INSTANCE: AgendaDatabase? = null
-
-        fun getDatabase(context: Context):AgendaDatabase{
-
-            val tempInstance = INSTANCE
-            if(tempInstance != null){
-                return tempInstance
-
-            }
-            synchronized(this){
-                val instance =  Room.databaseBuilder(
-                    context.applicationContext,
-                    AgendaDatabase::class.java,
-                    "Agenda_database"
-                ).build()
-
-                INSTANCE = instance
-                return instance
-            }
-        }
-
-    }
-}
+//package com.example.agenda_final.data
+//
+//import android.content.Context
+//import androidx.room.Database
+//import androidx.room.Room
+//import androidx.room.RoomDatabase
+//
+//
+//@Database(entities = arrayOf(Agenda::class),version = 1)
+//abstract class AgendaDatabase: RoomDatabase(){
+//
+//    abstract fun agendaDao():AgendaDao
+//
+//    companion object{
+//
+//        @Volatile
+//        private var INSTANCE: AgendaDatabase? = null
+//
+//        fun getDatabase(context: Context):AgendaDatabase{
+//
+//            val tempInstance = INSTANCE
+//            if(tempInstance != null){
+//                return tempInstance
+//
+//            }
+//            synchronized(this){
+//                val instance =  Room.databaseBuilder(
+//                    context.applicationContext,
+//                    AgendaDatabase::class.java,
+//                    "Agenda_database"
+//                ).build()
+//
+//                INSTANCE = instance
+//                return instance
+//            }
+//        }
+//
+//    }
+//}
